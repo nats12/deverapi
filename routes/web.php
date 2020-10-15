81ed30b665fc364b214b2becaa01ea86b75b397c
Natalie Mclaren <>
2020-10-15 14:43:59 +0200

    include web routes file

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->group(['prefix' => 'api', /*'middleware' => 'cors'*/], function () use ($router) {
+$router->group(['prefix' => 'api'], function () use ($router) {
 
     // FRAMEWORKS
     $router->get('frameworks',  ['uses' => 'FrameworkController@index']);
